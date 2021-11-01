caf2ca2e23134eea2327975e5adbed857e73130e
Chen-Yu Pao <windperson@gmail.com>
2021-11-02 02:40:27 +0800

    Add template generated files

---
+++ JumpyJetNet5.macOS/JumpyJetNet5App.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace JumpyJetNet5
+{
+    class JumpyJetNet5App
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
